6d51f346ae59124a4643a79493662f2db48bd594
ulysse-bouchet <ulysse.bouchet@pm.me>
2024-12-17 11:34:53 +0100

    Update texts

--- src/routes/(app)/api-usage/+page.svelte
+++ src/routes/(app)/api-usage/+page.svelte
@@ -8,19 +8,19 @@
 
 	const i18n = getContext('i18n'); // Translations
 
-	let tokensLeft = 0;
-	let tokensTotal = 10;
-	// Token spending data (note : we'll later have to fetch the data from the backend.)
-	let tokensSpent = {
+	let budgetLeft = 0;
+	let budgetTotal = 10;
+	// Budget spending data (note : we'll later have to fetch the data from the backend.)
+	let budgetSpent = {
 		timeValues: ['10/12', '11/12', '12/12', '13/12', '14/12', '15/12'],
-		promptTokens: [8, 8, 7, 6, 8, 9],
-		responseTokens: [5, 8, 9, 10, 7, 7]
+		promptSpending: [8, 8, 7, 6, 8, 9],
+		responseSpending: [5, 8, 9, 10, 7, 7]
 	};
 
 	subscriptionInfo.subscribe((info) => {
 		if (info.max_budget !== undefined && info.spend !== undefined) {
-			tokensLeft = info.max_budget - info.spend;
-			tokensTotal = info.max_budget;
+			budgetLeft = info.max_budget - info.spend;
+			budgetTotal = info.max_budget;
 		}
 	});
 </script>
@@ -33,9 +33,9 @@
 			class="bg-gray-50 dark:bg-gray-950 lg:w-72 md:h-96 rounded-3xl flex flex-col items-center p-6"
 		>
 			<!-- Tokens left -->
-			<h2 class="font-semibold">{$i18n.t('Tokens left')}</h2>
+			<h2 class="font-semibold">{$i18n.t('Budget left')}</h2>
 			<div class="w-64 h-64 p-6 relative">
-				<CircularProgressBar progress={(tokensLeft / tokensTotal) * 100} />
+				<CircularProgressBar progress={(budgetLeft / budgetTotal) * 100} />
 				<div
 					class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center"
 				>
@@ -44,17 +44,17 @@
 					>
 						<Coins className="size-6" />
 					</div>
-					<span class="font-bold text-3xl"> {tokensLeft}€ </span>
+					<span class="font-bold text-3xl"> {budgetLeft}€ </span>
 					<span class="font-medium text-xs">
 						<span class="text-[0.6rem]">/</span>
-						{tokensTotal}€
+						{budgetTotal}€
 					</span>
 				</div>
 			</div>
 			<button
 				class="bg-gray-100 rounded-full font-medium px-8 py-3 text-sm hover:bg-gray-200 transition"
 			>
-				{$i18n.t('Buy more')}
+				{$i18n.t('Refill')}
 			</button>
 		</div>
 
@@ -62,25 +62,25 @@
 		<div
 			class="bg-gray-50 dark:bg-gray-950 grow md:h-96 rounded-3xl flex flex-col items-center p-6"
 		>
-			<h2 class="font-semibold">{$i18n.t('Tokens spent')}</h2>
+			<h2 class="font-semibold">{$i18n.t('Spending history')}</h2>
 			<span class="text-xs">
-				{tokensSpent.timeValues[0]} - {tokensSpent.timeValues[tokensSpent.timeValues.length - 1]}
+				{budgetSpent.timeValues[0]} - {budgetSpent.timeValues[budgetSpent.timeValues.length - 1]}
 			</span>
 
 			<div class="w-full h-full pt-6 relative flex flex-col items-center">
 				<AreaLineChart
-					labels={tokensSpent.timeValues}
+					labels={budgetSpent.timeValues}
 					datasets={[
-						{ label: $i18n.t('Prompt tokens'), data: tokensSpent.promptTokens },
-						{ label: $i18n.t('Response tokens'), data: tokensSpent.responseTokens }
+						{ label: $i18n.t('Prompts'), data: budgetSpent.promptSpending },
+						{ label: $i18n.t('Responses'), data: budgetSpent.responseSpending }
 					]}
 				/>
 				<div class="pt-1 flex items-center space-x-2 text-xs font-medium">
 					<div class="rounded-full h-2 aspect-square bg-[#FF000055]"></div>
-					<span>{$i18n.t('Prompt tokens')}</span>
+					<span>{$i18n.t('Prompts')}</span>
 					<div class="w-8" />
 					<div class="rounded-full h-2 aspect-square bg-[#0000FF55]"></div>
-					<span>{$i18n.t('Response tokens')}</span>
+					<span>{$i18n.t('Responses')}</span>
 				</div>
 			</div>
 		</div>

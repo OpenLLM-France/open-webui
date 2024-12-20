21638102e4ecd18f9bfdd729abcdb334c4af9d92
ulysse-bouchet <ulysse.bouchet@pm.me>
2024-12-20 15:57:59 +0100

    Added a timer

---
+++ src/lib/components/icons/Timer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let time: number = 5;
+
+	const timer = setInterval(() => {
+		time -= 1;
+		if (time <= 0) {
+			clearInterval(timer);
+			setTimeout(() => {
+				alert('You ran out of time');
+			}, 25); // To ensure the timer is visually 0
+		}
+	}, 1000);
+</script>
+
+<div class="flex items-center space-x-2">
+	<span class="text-sm"> Time remaining : {time}s </span>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="24px"
+		viewBox="0 -960 960 960"
+		width="24px"
+		fill="currentColor"
+	>
+		<path
+			d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z"
+		/>
+	</svg>
+</div>

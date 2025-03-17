798f037cb04bc7cb5fc50124074bfc215ca69ab9
ulysse-bouchet <ulysse.bouchet@pm.me>
2025-03-17 12:38:45 +0100

    Update footer logo

--- src/lib/components/layout/Landing/Footer.svelte
+++ src/lib/components/layout/Landing/Footer.svelte
@@ -7,18 +7,18 @@
 	<div class="h-px w-full my-4 bg-gray-300" />
 	<img
 		crossorigin="anonymous"
-		src="/assets/logos/openllm-france-horizontal.svg"
-		class="h-16"
-		alt="OpenLLM France logo"
+		src="/assets/logos/linagora-ai.png"
+		class="h-12 my-4"
+		alt="Linagora AI logo"
 	/>
 	<div class="flex flex-col items-center space-y-2">
 		<span class="text-xs">2025 - {$i18n.t('All rights reserved')} ©</span>
 		<div class="text-[0.6rem]">
-			<a href="https://linagora.com/fr/legal" class="underline hover:text-violet-500">
+			<a href="https://linagora.com/fr/legal" class="underline hover:text-rose-500">
 				{$i18n.t('Terms of Use')}
 			</a>
 			-
-			<a href="https://linagora.com/fr/privacy" class="underline hover:text-violet-500">
+			<a href="https://linagora.com/fr/privacy" class="underline hover:text-rose-500">
 				{$i18n.t('Privacy Policy')}
 			</a>
 		</div>
